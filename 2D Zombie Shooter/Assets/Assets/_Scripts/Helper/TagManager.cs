e0e8e1d825cc0b42b7c9994b94a2680f7bb7b091
Sunil pargi <sunilpargi121@gmail.com>
2020-10-18 17:31:33 +0530

    added player move

---
+++ 2D Zombie Shooter/Assets/Assets/_Scripts/Helper/TagManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagManager 
+{
+    //Tags
+    public static string PLAYER_TAG = "Player";
+    public static string PLAYER_HEALTH_TAG = "PlayerHealth";
+    public static string BACKGROUND_TAG = "Background";
+    public static string BULLET_TAG = "Bullet";
+    public static string FENCE_TAG = "Fence";
+    public static string ROCKET_MISSILE_TAG = "RocketMissile";
+    public static string FIRE_BULLET_TAG = "FireBullet";
+    public static string ZOMBIE_HEALTH_TAG = "ZombieHealth";
+    public static string MELLE_WEAPON_TAG = "MeleeWeapon";
+    public static string COIN_TAG = "Coin";
+
+    //NAMES
+    public static string RIGHT_ARM = "Arm1";
+
+    //ANIMATION AND TRIGGER
+    public static string RUN_PARAMETER = "Run";
+    public static string ATTACK_PARAMETER = "Attack";
+    public static string TYPE_WEAPON_PARAMETER = "TypeWeapon";
+    public static string SWITCH_PARAMETER = "Switch";
+    public static string GET_HURT_PARAMETER = "GetHurt";
+    public static string DEAD_PARAMETER = "Dead";
+    public static string RANDOM_PARAMETER = "Random";
+    public static string ZOMBIE_MOVE_PARAMETER = "Move";
+}
